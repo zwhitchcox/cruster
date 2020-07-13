5703b1dfd63a5d72b21502cfbb4e05d4f4b0485a
Zane Hitchcox <zwhitchcox@gmail.com>
2020-07-13 11:07:13 -0400

    add kubernetes

---
+++ README.md
@@ -0,0 +1,4 @@
+Run:
+```sh
+sudo sh -c "$(curl -fsSL https://raw.github.com/zwhitchcox/cluster-pi/master/master-node.sh)"
+```
--- master-node.sh
+++ master-node.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [[ "$(whoami)" != 'root' ]] ; then echo "Must be run with 'sudo'" ; fi
+if [[ "$(whoami)" != 'root' ]] ; then echo "Must be run with 'sudo'" && exit 1 ; fi
 
 # install docker and enable on boot
-sudo apt install docker.io -y
-sudo service docker start
-sudo systemctl enable docker
+apt install docker.io -y
+service docker start
+systemctl enable docker
 
 # change cgroups driver to systemd
-sudo cat > /etc/docker/daemon.json <<EOF
+cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
@@ -18,3 +18,23 @@ sudo cat > /etc/docker/daemon.json <<EOF
 }
 EOF
 
+# enable cgroups limit support
+sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt
+
+# Enable net.bridge.bridge-nf-call-iptables and -iptables6
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+# Add the packages.cloud.google.com atp key
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Add the Kubernetes repo
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+# Update the apt cache and install kubelet, kubeadm, and kubectl
+# (Output omitted)
+apt update && apt install -y kubelet kubeadm kubectl
