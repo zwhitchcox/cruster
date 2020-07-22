35cdcd35f3dfbb613272e3ecf879b046c58d7b06
Zane Hitchcox <zwhitchcox@gmail.com>
2020-07-21 21:12:09 -0400

    switch to raspbian lite

--- create-cluster.bash
+++ create-cluster.bash
@@ -10,9 +10,9 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# bash "${HOSTDIR}/img-common-create.bash" 
+bash "${HOSTDIR}/img-common-create.bash" 
 bash "${HOSTDIR}/img-mount.bash" common
-# bash "${HOSTDIR}/common-provision.bash"
+bash "${HOSTDIR}/common-provision.bash"
 
 chroot /mnt/common /bin/bash -c "echo master > /etc/hostname"
 cp ${IMGDIR}/common.img ${IMGDIR}/master.img
@@ -23,4 +23,4 @@ for (( c=$START; c<=$NUMSLAVES; c++ )) do
     cp ${IMGDIR}/common.img ${IMGDIR}/slave-${c}.img
 done
 
-# bash "${HOSTDIR}/img-umount.bash" common
+bash "${HOSTDIR}/img-umount.bash" common
