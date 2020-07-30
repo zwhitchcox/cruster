c6f9b191e54dde461eb6a42fb861212f700dcac2
Zane Hitchcox <zwhitchcox@gmail.com>
2020-07-30 10:19:02 -0400

    can search

--- desktop/main.js
+++ desktop/main.js
@@ -1,22 +1,83 @@
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
+const electron = require('electron');
+const {ipcMain} = electron
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
 const path = require('path');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width:800,
-        height:600,
-        show: false
-    });
-    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
-
-    mainWindow.loadURL(startURL);
-
-    mainWindow.once('ready-to-show', () => mainWindow.show());
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: __dirname + '/preload.js'
+    }
+  });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  if (isDev) {
+    // Open the DevTools.
+    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+    mainWindow.webContents.openDevTools();
+  }
+  mainWindow.on('closed', () => mainWindow = null);
 }
+
 app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+var Client = require('node-ssdp').Client
+  , client = new Client();
+
+client.on('response', function (headers, statusCode, rinfo) {
+  mainWindow.webContents.send("node-response", headers)
+});
+
+ipcMain.on('search-nodes', (event, arg) => {
+  client.search('picluster:node');
+})
+// const { app, BrowserWindow, ipcMain } = require('electron');
+// const isDev = require('electron-is-dev');
+// const path = require('path');
+
+// let mainWindow;
+
+// function createWindow() {
+//     mainWindow = new BrowserWindow({
+//         width:800,
+//         height:600,
+//         show: false
+//     });
+//     const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
+
+//     mainWindow.loadURL(startURL);
+
+//     mainWindow.once('ready-to-show', () => mainWindow.show());
+//     mainWindow.on('closed', () => {
+//         mainWindow = null;
+//     });
+// }
+// app.on('ready', createWindow);
+
+// var Client = require('node-ssdp').Client
+//   , client = new Client();
+// client.on('response', function (headers, statusCode, rinfo) {
+//   console.log('Got a response to an m-search.');
+// });
+// ipcMain.on('search-nodes', () => {
+//   client.search('ssdp:all');
+// })
--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+declare var ipcRenderer;
+
 function App() {
+  const [nodes, setNodes] = useState([])
+  useEffect(() => {
+    ipcRenderer.on('node-response', (event, headers)=> {
+      // console.log(headers["ST"])
+      if (/cluster/.test(headers["ST"])) {
+        console.log(headers)
+      }
+      if (headers["ST"] && headers["ST"] == "picluster:node") {
+        setNodes(headers["LOCATION"])
+      }
+    })
+  }, [])
+  const search = () => {
+    setNodes([])
+    ipcRenderer.send('search-nodes')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        hello
+        <button onClick={search}>Search</button>
       </header>
     </div>
   );
