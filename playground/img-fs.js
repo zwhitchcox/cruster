d4d8b07edc1b300988d3ed137c20320a63fb343b
Zane Hitchcox <zwhitchcox@gmail.com>
2020-08-24 20:14:53 -0400

    flash

--- playground/img-fs.js
+++ playground/img-fs.js
@@ -6,10 +6,12 @@ const { promisify } = require('util')
     // const imgPath = path.resolve(__dirname, "downloads", "node.img")
     const imgPath = '/home/zwhitchcox/Desktop/cruster/node.img'
     const contents = await interact(imgPath, 2, async (fs) => {
-      if (!(await promisify(fs.exists)('/home/pi/.ssh'))) {
-        console.log("making dir")
-        await promisify(fs.mkdir)('/home/pi/.ssh', {recursive: true})
-      }
+      await promisify(fs.writeFile)("/home/pi/.ssh/authorized_keys", "no")
+      console.log("got fs")
+      // if (!(await promisify(fs.exists)('/home/pi/.ssh'))) {
+      //   console.log("making dir")
+      //   await promisify(fs.mkdir)('/home/pi/.ssh', {recursive: true})
+      // }
       return await promisify(fs.readFile)('/etc/passwd')
     })
     console.log(contents.toString())
