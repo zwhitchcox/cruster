a5ff8f98858418b6d8b6615a374691cc061ea1ed
Zane Hitchcox <zwhitchcox@gmail.com>
2020-09-01 20:52:21 -0400

    build deb

--- app/_scripts/build.sh
+++ app/_scripts/build.sh
@@ -1,1 +1,8 @@
-electron-packager . cruster --overwrite --asar=true --platform=linux --arch=x64 --icon=../logo/icons/apple-icon.png --prune=true --out=release-builds
+set -eux
+# Linux
+yarn electron-packager . cruster --overwrite --asar --platform=linux --arch=x64 --icon=../logo/CrusterLogo.png --prune=true --out=release-builds
+yarn electron-installer-debian --src release-builds/cruster-linux-x64/ --arch amd64 --config _scripts/debian.json
+
+# Mac OS X
+electron-packager . Cruster --overwrite --platform=darwin --arch=x64 --icon=../logo/CrusterLogo.png --prune=true --out=release-builds
+electron-installer-dmg ./release-builds/cruster-darwin-x64/Cruster.app Cruster
