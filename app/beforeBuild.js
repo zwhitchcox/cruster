31452fba5ee638f88f81ca1d307a5a99674af852
Zane Hitchcox <zwhitchcox@gmail.com>
2020-09-04 21:01:25 -0400

    can build

--- app/beforeBuild.js
+++ app/beforeBuild.js
@@ -5,10 +5,10 @@ const process = require('process');
 
 // Rebuild native modules for ia32 and run webpack again for the ia32 part of windows packages
 exports.default = function(context) {
-  if (context.platform.name === 'windows') {
-    cp.execFileSync(
-      'bash',
-      ['./node_modules/.bin/electron-rebuild', '--types', 'dev', '--arch', context.arch],
-    );
-  }
+  // if (context.platform.name === 'windows') {
+  //   cp.execFileSync(
+  //     'bash',
+  //     ['./node_modules/.bin/electron-rebuild', '--types', 'dev', '--arch', context.arch],
+  //   );
+  // }
 }
