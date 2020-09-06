dfbad2fb9b56034e77505d2949641442531b6acb
Zane Hitchcox <zwhitchcox@gmail.com>
2020-09-05 21:56:20 -0400

    push asset script

--- app/_scripts/publish.sh
+++ app/_scripts/publish.sh
@@ -1,4 +1,4 @@
-yarn build
+# yarn build
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux;;
@@ -8,11 +8,17 @@ case "${unameOut}" in
     *)          machine="UNKNOWN:${unameOut}"
 esac
 if [ "${machine}" == "Linux" ]; then
+pushd dist > /dev/null
 token=$(cat ${HOME}/.PAT)
-exit
-github-release upload \
-  --owner zwhitchcox \
-  --repo cruster \
-  --tag "v0.0.1"
-  cruster
+# github-release upload \
+#   --owner zwhitchcox \
+#   --repo cruster \
+#   --token ${token} \
+#   --tag "v0.0.1" \
+#   --name "Initial Release" \
+#   --prerelease \
+#   cruster-0.1.0.AppImage
+mv cruster-0.1.0.AppImage Linux-cruster-0.1.0.AppImage
+bash ../_scripts/upload-asset.sh owner=zwhitchcox repo=cruster tag="v0.0.1" filename=Linux-cruster-0.1.0.AppImage github_api_token=${token}
 fi
+popd > /dev/null
