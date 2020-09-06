ffdc8c63f85e769efdbe6fedda550bf9a445b924
Zane Hitchcox <zwhitchcox@gmail.com>
2020-09-05 22:47:19 -0400

    publish script mac

--- app/_scripts/publish.sh
+++ app/_scripts/publish.sh
@@ -1,4 +1,4 @@
-# yarn build
+yarn build
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux;;
@@ -7,18 +7,17 @@ case "${unameOut}" in
     MINGW*)     machine=MinGw;;
     *)          machine="UNKNOWN:${unameOut}"
 esac
-if [ "${machine}" == "Linux" ]; then
 pushd dist > /dev/null
-token=$(cat ${HOME}/.PAT)
-# github-release upload \
-#   --owner zwhitchcox \
-#   --repo cruster \
-#   --token ${token} \
-#   --tag "v0.0.1" \
-#   --name "Initial Release" \
-#   --prerelease \
-#   cruster-0.1.0.AppImage
-mv cruster-0.1.0.AppImage Linux-cruster-0.1.0.AppImage
-bash ../_scripts/upload-asset.sh owner=zwhitchcox repo=cruster tag="v0.0.1" filename=Linux-cruster-0.1.0.AppImage github_api_token=${token}
+if [ "${machine}" == "Linux" ]; then
+  token=$(cat ${HOME}/.PAT)
+  mv cruster-0.1.0.AppImage Linux-cruster-0.1.0.AppImage
+  bash ../_scripts/upload-asset.sh owner=zwhitchcox repo=cruster tag="v0.0.1" filename=Linux-cruster-0.1.0.AppImage github_api_token=${token}
+fi
+if [ "${machine}" == "Mac" ]; then
+  token=$(cat ${HOME}/.PAT)
+notarize_password=$(cat ~/.NotarizePassword)
+mv cruster-0.1.0.dmg Mac-OS-X-cruster-0.1.0.dmg
+xcrun altool --notarize-app -f ./Mac-OS-X-cruster-0.1.0.dmg  --primary-bundle-id io.ivan.cruster -u zwhitchcox@me.com -p ${notarize_password}
+  bash ../_scripts/upload-asset.sh owner=zwhitchcox repo=cruster tag="v0.0.1" filename=Mac-OS-X-cruster-0.1.0.dmg github_api_token=${token}
 fi
 popd > /dev/null
