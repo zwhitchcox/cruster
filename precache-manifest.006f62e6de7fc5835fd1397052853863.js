a46167d40e447157120b82aa9f1e99f55c3072bf
Zane Hitchcox <zwhitchcox@gmail.com>
2020-09-07 14:09:05 -0400

    Updates

---
+++ precache-manifest.006f62e6de7fc5835fd1397052853863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcb11571389d4500e68c684ae4e0d1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d33604d7cf2574cf1f3",
+    "url": "/static/css/main.85d12d8d.chunk.css"
+  },
+  {
+    "revision": "5d709dfbb919c0275095",
+    "url": "/static/js/2.8a0e0828.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8a0e0828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d33604d7cf2574cf1f3",
+    "url": "/static/js/main.9dbf448c.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  }
+]);
